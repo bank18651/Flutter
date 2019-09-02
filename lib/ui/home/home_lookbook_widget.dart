cb844107fa8f8fcb9a9d4341a68a0507d1ec39fa
sujin <sujin.c@pomelofashion.com>
2019-09-02 12:10:58 +0700

    lookbook layout

--- lib/ui/home/home_lookbook_widget.dart
+++ lib/ui/home/home_lookbook_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pomelo_flutter/data/feed/feed_lookbook_item.dart';
 import 'package:pomelo_flutter/ui/imgix_image_widget.dart';
+
 class HomeLookbookWidget extends StatelessWidget {
   final FeedLookbookItem lookbook;
 
@@ -9,21 +10,30 @@ class HomeLookbookWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(
-      padding: EdgeInsets.all(15),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          AspectRatio(
-            aspectRatio: 4 / 3,
-            child: ImgixImageWidget(imageUrl: lookbook.image.url),
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 4 / 3,
+                child: ImgixImageWidget(imageUrl: lookbook.image.url),
+              ),
+              SizedBox(height: 8),
+              Text(lookbook.title),
+              SizedBox(height: 8),
+              Text(lookbook.releaseDate),
+            ],
           ),
-          SizedBox(height: 8),
-          Text(lookbook.title),
-          SizedBox(height: 8),
-          Text(lookbook.releaseDate),
-        ],
-      ),
+        ),
+        Container(
+          height: 10,
+          decoration:
+          BoxDecoration(color: Color.fromRGBO(211, 211, 211, 1)),
+        )
+      ],
     );
   }
 }
